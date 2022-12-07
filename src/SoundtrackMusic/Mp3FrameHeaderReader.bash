9cfccaae4b5e9f6b7589a7bc444fbe20eed96b82
Eric Schlichting <ericschlichting@gmail.com>
2022-12-06 21:33:41 -0600

    Support for bash v3

--- src/SoundtrackMusic/Mp3FrameHeaderReader.bash
+++ src/SoundtrackMusic/Mp3FrameHeaderReader.bash
@@ -17,55 +17,238 @@ function hex_str_to_int() {
   echo $(printf "%d" 0x$1)
 }
 
-declare -A mpeg_version_map=(
-  [0]=3 # MPEG 2.5
-  # 1 is reserved
-  [2]=2 # MPEG 2
-  [3]=1 # MPEG 1
-)
+# Form of (layer) as arg 1
+function padding_map() {
+  if [ $1 -eq 0 ]
+  then
+    echo 0
+  else
+    case $2 in
+      '1') echo 4;;
+      '2') echo 1;;
+      '3') echo 1;;
+    esac
+  fi
+}
 
-declare -A layer_map=(
-  [1]=3 # Layer III
-  [2]=2 # Layer II
-  [3]=1 # Layer I
-)
+function mpeg_version_map() {
+  case $1 in 
+    '0') echo 3;; # MPEG 2.5
+    # 1 is reserved
+    '2') echo 2;; # MPEG 2
+    '3') echo 1;; # MPEG 1
+  esac
+}
+
+function layer_map() {
+  case $1 in
+    # 0 is reserved
+    '1') echo 3;; # Layer III
+    '2') echo 2;; # Layer II
+    '3') echo 1;; # Layer I
+  esac
+}
 
 # Constants for sampling rate lookup
 # Form of  (sampling_rate_index)_(mpeg_version)
 # for simplicity mpeg 2.5 is 3 in the map
-declare -A sampling_rate_map=(
-  ["0_1"]=44100 # index 0 mpeg 1
-  ["0_2"]=22050 # index 0 mpeg 2
-  ["0_3"]=11025 # index 0 mpeg 2.5
+function sampling_rate_map() {
+  case $1 in
+    '0_1') echo 44100;; # index 0 mpeg 1
+    '0_2') echo 22050;; # index 0 mpeg 2
+    '0_3') echo 11025;; # index 0 mpeg 2.5
 
-  ["1_1"]=48000 # index 1 mpeg 1
-  ["1_2"]=24000 # index 1 mpeg 2
-  ["1_3"]=12000 # index 1 mpeg 2.5
+    '1_1') echo 48000;; # index 1 mpeg 1
+    '1_2') echo 24000;; # index 1 mpeg 2
+    '1_3') echo 12000;; # index 1 mpeg 2.5
 
-  ["2_1"]=32000 # index 2 mpeg 1
-  ["2_2"]=16000 # index 2 mpeg 2
-  ["2_3"]=8000 # index 2 mpeg 2.5
-)
+    '2_1') echo 32000;; # index 2 mpeg 1
+    '2_2') echo 16000;; # index 2 mpeg 2
+    '2_3') echo 8000;; # index 2 mpeg 2.5
+  esac
+}
+
+#           MPEG1	MPEG2	MPEG2.5
+# Layer I	  384	  384 	384
+# Layer II	1152 	1152	1152
+# Layer III 1152 	576 	576
+# Form of (mpeg_version)_(layer)
+# for simplicity mpeg 2.5 is 3 in the map
+function samples_per_frame_map() {
+  case $1 in
+    '1_1') echo 384;;
+    '1_2') echo 1152;;
+    '1_3') echo 1152;;
+
+    '2_1') echo 384;;
+    '2_2') echo 1152;;
+    '2_3') echo 576;;
+
+    '3_1') echo 384;;
+    '3_2') echo 1152;;
+    '3_3') echo 576;;
+  esac
+}
 
 # Constants for bit rate lookup
 # Form of  (bit_rate_index)_(mpeg_version)_(layer)
 # for simplicity mpeg 2.5 is 3 in the map
-declare -A bit_rate_map=(
-  ["1_1_1"]=32 ["1_1_2"]=32 ["1_1_3"]=32 ["1_2_1"]=32 ["1_2_2"]=8 ["1_2_3"]=8 ["1_3_1"]=32 ["1_3_2"]=8 ["1_3_3"]=8
-  ["2_1_1"]=64 ["2_1_2"]=48 ["2_1_3"]=40 ["2_2_1"]=48 ["2_2_2"]=16 ["2_2_3"]=16 ["2_3_1"]=48 ["2_3_2"]=16 ["2_3_3"]=16
-  ["3_1_1"]=96 ["3_1_2"]=56 ["3_1_3"]=48 ["3_2_1"]=56 ["3_2_2"]=24 ["3_2_3"]=24 ["3_3_1"]=56 ["3_3_2"]=24 ["3_3_3"]=24
-  ["4_1_1"]=128 ["4_1_2"]=64 ["4_1_3"]=56 ["4_2_1"]=64 ["4_2_2"]=32 ["4_2_3"]=32 ["4_3_1"]=64 ["4_3_2"]=63 ["4_3_3"]=32
-  ["5_1_1"]=160 ["5_1_2"]=80 ["5_1_3"]=64 ["5_2_1"]=80 ["5_2_2"]=40 ["5_2_3"]=40 ["5_3_1"]=80 ["5_3_2"]=40 ["5_3_3"]=40
-  ["6_1_1"]=192 ["6_1_2"]=96 ["6_1_3"]=80 ["6_2_1"]=96 ["6_2_2"]=48 ["6_2_3"]=48 ["6_3_1"]=96 ["6_3_2"]=48 ["6_3_3"]=48
-  ["7_1_1"]=224 ["7_1_2"]=112 ["7_1_3"]=96 ["7_2_1"]=112 ["7_2_2"]=56 ["7_2_3"]=56 ["7_3_1"]=112 ["7_3_2"]=56 ["7_3_3"]=56
-  ["8_1_1"]=256 ["8_1_2"]=128 ["8_1_3"]=112 ["8_2_1"]=128 ["8_2_2"]=64 ["8_2_3"]=64 ["8_3_1"]=128 ["8_3_2"]=64 ["8_3_3"]=64
-  ["9_1_1"]=288 ["9_1_2"]=160 ["9_1_3"]=128 ["9_2_1"]=144 ["9_2_2"]=80 ["9_2_3"]=80 ["9_3_1"]=144 ["9_3_2"]=80 ["9_3_3"]=80
-  ["10_1_1"]=320 ["10_1_2"]=192 ["10_1_3"]=160 ["10_2_1"]=160 ["10_2_2"]=96 ["10_2_3"]=96 ["10_3_1"]=160 ["10_3_2"]=96 ["10_3_3"]=96
-  ["11_1_1"]=352 ["11_1_2"]=224 ["11_1_3"]=192 ["11_2_1"]=176 ["11_2_2"]=112 ["11_2_3"]=112 ["11_3_1"]=176 ["11_3_2"]=112 ["11_3_3"]=112
-  ["12_1_1"]=384 ["12_1_2"]=256 ["12_1_3"]=224 ["12_2_1"]=192 ["12_2_2"]=128 ["12_2_3"]=128 ["12_3_1"]=192 ["12_3_2"]=128 ["12_3_3"]=128
-  ["13_1_1"]=416 ["13_1_2"]=320 ["13_1_3"]=256 ["13_2_1"]=224 ["13_2_2"]=144 ["13_2_3"]=144 ["13_3_1"]=224 ["13_3_2"]=144 ["13_3_3"]=144
-  ["14_1_1"]=448 ["14_1_2"]=384 ["14_1_3"]=320 ["14_2_1"]=256 ["14_2_2"]=160 ["14_2_3"]=160 ["14_3_1"]=256 ["14_3_2"]=160 ["14_3_3"]=160
-)
+function bit_rate_map() {
+  case $1 in
+    '1_1_1') echo 32;;
+    '1_1_2') echo 32;;
+    '1_1_3') echo 32;;
+    '1_2_1') echo 32;;
+    '1_2_2') echo 8;;
+    '1_2_3') echo 8;;
+    '1_3_1') echo 32;;
+    '1_3_2') echo 8;;
+    '1_3_3') echo 8;;
+
+
+    '2_1_1') echo 64;;
+    '2_1_2') echo 48;;
+    '2_1_3') echo 40;;
+    '2_2_1') echo 48;;
+    '2_2_2') echo 16;;
+    '2_2_3') echo 16;;
+    '2_3_1') echo 48;;
+    '2_3_2') echo 16;;
+    '2_3_3') echo 16;;
+
+
+    '3_1_1') echo 96;;
+    '3_1_2') echo 56;;
+    '3_1_3') echo 48;;
+    '3_2_1') echo 56;;
+    '3_2_2') echo 24;;
+    '3_2_3') echo 24;;
+    '3_3_1') echo 56;;
+    '3_3_2') echo 24;;
+    '3_3_3') echo 24;;
+
+
+    '4_1_1') echo 128;;
+    '4_1_2') echo 64;;
+    '4_1_3') echo 56;;
+    '4_2_1') echo 64;;
+    '4_2_2') echo 32;;
+    '4_2_3') echo 32;;
+    '4_3_1') echo 64;;
+    '4_3_2') echo 63;;
+    '4_3_3') echo 32;;
+
+
+    '5_1_1') echo 160;;
+    '5_1_2') echo 80;;
+    '5_1_3') echo 64;;
+    '5_2_1') echo 80;;
+    '5_2_2') echo 40;;
+    '5_2_3') echo 40;;
+    '5_3_1') echo 80;;
+    '5_3_2') echo 40;;
+    '5_3_3') echo 40;;
+
+
+    '6_1_1') echo 192;;
+    '6_1_2') echo 96;;
+    '6_1_3') echo 80;;
+    '6_2_1') echo 96;;
+    '6_2_2') echo 48;;
+    '6_2_3') echo 48;;
+    '6_3_1') echo 96;;
+    '6_3_2') echo 48;;
+    '6_3_3') echo 48;;
+
+
+    '7_1_1') echo 224;;
+    '7_1_2') echo 112;;
+    '7_1_3') echo 96;;
+    '7_2_1') echo 112;;
+    '7_2_2') echo 56;;
+    '7_2_3') echo 56;;
+    '7_3_1') echo 112;;
+    '7_3_2') echo 56;;
+    '7_3_3') echo 56;;
+
+
+    '8_1_1') echo 256;;
+    '8_1_2') echo 128;;
+    '8_1_3') echo 112;;
+    '8_2_1') echo 128;;
+    '8_2_2') echo 64;;
+    '8_2_3') echo 64;;
+    '8_3_1') echo 128;;
+    '8_3_2') echo 64;;
+    '8_3_3') echo 64;;
+
+
+    '9_1_1') echo 288;;
+    '9_1_2') echo 160;;
+    '9_1_3') echo 128;;
+    '9_2_1') echo 144;;
+    '9_2_2') echo 80;;
+    '9_2_3') echo 80;;
+    '9_3_1') echo 144;;
+    '9_3_2') echo 80;;
+    '9_3_3') echo 80;;
+
+
+    '10_1_1') echo 320;;
+    '10_1_2') echo 192;;
+    '10_1_3') echo 160;;
+    '10_2_1') echo 160;;
+    '10_2_2') echo 96;;
+    '10_2_3') echo 96;;
+    '10_3_1') echo 160;;
+    '10_3_2') echo 96;;
+    '10_3_3') echo 96;;
+
+
+    '11_1_1') echo 352;;
+    '11_1_2') echo 224;;
+    '11_1_3') echo 192;;
+    '11_2_1') echo 176;;
+    '11_2_2') echo 112;;
+    '11_2_3') echo 112;;
+    '11_3_1') echo 176;;
+    '11_3_2') echo 112;;
+    '11_3_3') echo 112;;
+
+
+    '12_1_1') echo 384;;
+    '12_1_2') echo 256;;
+    '12_1_3') echo 224;;
+    '12_2_1') echo 192;;
+    '12_2_2') echo 128;;
+    '12_2_3') echo 128;;
+    '12_3_1') echo 192;;
+    '12_3_2') echo 128;;
+    '12_3_3') echo 128;;
+
+
+    '13_1_1') echo 416;;
+    '13_1_2') echo 320;;
+    '13_1_3') echo 256;;
+    '13_2_1') echo 224;;
+    '13_2_2') echo 144;;
+    '13_2_3') echo 144;;
+    '13_3_1') echo 224;;
+    '13_3_2') echo 144;;
+    '13_3_3') echo 144;;
+
+
+    '14_1_1') echo 448;;
+    '14_1_2') echo 384;;
+    '14_1_3') echo 320;;
+    '14_2_1') echo 256;;
+    '14_2_2') echo 160;;
+    '14_2_3') echo 160;;
+    '14_3_1') echo 256;;
+    '14_3_2') echo 160;;
+    '14_3_3') echo 160;;
+  esac
+}
 
 file=$1
 
@@ -116,12 +299,12 @@ do
 
       mpeg_version_bit_2=$((($character_four_int & 2#1000) >> 3))
       mpeg_version_index=$((($mpeg_version_bit_1 << 1) + $mpeg_version_bit_2))
-      mpeg_version="${mpeg_version_map[$mpeg_version_index]}"
+      mpeg_version=$(mpeg_version_map $mpeg_version_index)
 
       layer_index=$((($character_four_int & 2#0110) >> 1))
-      layer="${layer_map[$layer_index]}"
+      layer=$(layer_map $layer_index)
 
-      protected=$(($character_four_int & 2#0001))
+      protected_bit=$(($character_four_int & 2#0001))
     }
 
     # Hex character 5 represents: Bit rate index, need to lookup value into table of constants
@@ -130,7 +313,8 @@ do
       character_five_int=$(hex_str_to_int $hex_character_five)
 
       bit_rate_key="${character_five_int}_${mpeg_version}_${layer}"
-      bit_rate=${bit_rate_map[$bit_rate_key]}
+      kbit_rate=$(bit_rate_map $bit_rate_key)
+      bit_rate=$((kbit_rate * 1000))
     }
 
     # Hex character 6 represents: 2 bits for sampling rate index, 1 bit for padding, 1 bit for private bit
@@ -140,9 +324,10 @@ do
 
       sampling_rate_index=$((($character_six_int & 2#1100) >> 2))
       sampling_rate_key="${sampling_rate_index}_${mpeg_version}"
-      sampling_rate=${sampling_rate_map[$sampling_rate_key]}
+      sampling_rate=$(sampling_rate_map $sampling_rate_key)
 
       padding_bit=$((($character_six_int & 2#0010) >> 1))
+      padding_amount_bytes=$(padding_map $padding_bit $layer)
 
       private_bit=$(($character_six_int & 2#0001))
     }
@@ -169,18 +354,25 @@ do
       emphasis=$(($character_eight_int & 2#0011))
     }
 
-    debug "  ($hex_character_four) MPEG version: $mpeg_version, Layer: $layer, Protected: $protected"
-    debug "  ($hex_character_five) Bit rate: (key = $bit_rate_key) $bit_rate kb/s"
-    debug "  ($hex_character_six) Sampling rate: (key = $sampling_rate_key) ${sampling_rate}Hz, Padding: $padding_bit, Private: $private_bit"
+    samples_per_frame_key="${mpeg_version}_${layer}"
+    samples_per_frame=$(samples_per_frame_map $samples_per_frame_key)
+
+    debug "  ($hex_character_four) MPEG version: $mpeg_version, Layer: $layer, Protected bit: $protected_bit"
+    debug "  ($hex_character_five) Bit rate: (key = $bit_rate_key) $kbit_rate kb/s"
+    debug "  ($hex_character_six) Sampling rate: (key = $sampling_rate_key) ${sampling_rate}Hz, Padding: $padding_bit, Padding amount bytes: $padding_amount_bytes, Private: $private_bit"
     debug "  ($hex_character_seven) Channel mode: $channel_mode, Channel mode extension: $channel_mode_extension"
     debug "  ($hex_character_eight) Copyright: $copyright_bit, Original: $original_bit, Emphasis: $emphasis"
 
-    if [ $mpeg_version -ne 1 ] || [ $layer -eq 0 ]
-    then
-      debug "  This is not an mpeg1 frame header, moving to next word boundary"
-      i=$(($i + 4))
-      continue
-    fi
+    # Frame Size = ( (Samples Per Frame / 8 * Bitrate) / Sampling Rate) + Padding Size
+    frame_size_bytes=$(((($samples_per_frame / 8 * $bit_rate) / $sampling_rate) + $padding_amount_bytes))
+
+    debug "  Samples per frame: $samples_per_frame, Frame size bytes: $frame_size_bytes"
+
+    # Duration = File Size / Bitrate * 8
+    file_size_bytes=$(($file_size_characters / 2))
+    duration=$(($file_size_bytes / $bit_rate * 8))
+
+    debug "  File size bytes: $file_size_bytes, File duration: $duration seconds"
 
     exit
   else
