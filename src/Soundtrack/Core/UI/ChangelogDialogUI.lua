2acb1a9e6716359ab08d02ba9b2315862b1a5cf6
Eric Schlichting <ericschlichting@gmail.com>
2026-04-21 18:44:42 -0500

    feat: Update version to 6.5.0 and enhance compatibility with World of Warcraft 12.0.5

--- src/Soundtrack/Core/UI/ChangelogDialogUI.lua
+++ src/Soundtrack/Core/UI/ChangelogDialogUI.lua
@@ -4,8 +4,7 @@ local CURRENT_VERSION = C_AddOns.GetAddOnMetadata("Soundtrack", "Version")
 
 local CHANGELOG_TEXT = [[What's new in ]] .. CURRENT_VERSION .. [[:
 
-- New themed UI with customizable color themes (Gold, Cosmic Blue, Class Color, Forest, Crimson, Arcane). Change your theme in the Options tab.
-- New Encounters tab replaces Boss Zones. Encounters are automatically added when you enter a boss fight, and you can assign custom music to each one. Use the new Collapse All / Expand All buttons to manage the list.
+- Added support for the latest version of World of Warcraft.
 ]]
 
 function Soundtrack.ChangelogDialog.CheckAndShow()
